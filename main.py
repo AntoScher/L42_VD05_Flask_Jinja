596255fd08a42356f02bd8f37513cc9a792aaea6
AntoScher <asc8er@gmail.com>
2024-12-13 21:58:10 +0300

    create films

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def films():
-    return render_template("index.html")
+    context = {
+        "caption": "Фильмы ",
+        "link": "Перейти куда-нибудь"
+    }
+    return render_template("index.html", **context)
+
 
 @app.route("/person/")
 def person():
-    return render_template("main.html")
+    return render_template("main.html", caption="Герои фильмов", link="Перейти? ")
+
 
 if __name__ == "__main__":
     app.run()
