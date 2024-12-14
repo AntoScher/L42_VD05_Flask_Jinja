9e692e22cc5539ecbfd31af1d07b928208757bb3
AntoScher <asc8er@gmail.com>
2024-12-14 12:47:21 +0300

    pre final

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 def films():
     context = {
         "caption": "Фильмы ",
-        "link": "Перейти куда-нибудь"
+        "link": "Перейти куда-нибудь",
+
     }
     return render_template("index.html", **context)
 
 
 @app.route("/person/")
 def person():
-    return render_template("main.html", caption="Герои фильмов", link="Перейти? ")
+    return render_template("main.html", caption="Герои фильмов", link="Вернуться? ")
 
 
 if __name__ == "__main__":
