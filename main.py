8c5e6b66688edf1d3625ad6a608c4dc86ed77947
AntoScher <asc8er@gmail.com>
2024-12-17 08:31:44 +0300

    krivoe derevo

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/")
-def films():
-    context = {
-        "caption": "Фильмы ",
-        "link": "Перейти на страницу героев фильма?",
-    }
-    return render_template("index.html", **context)
-@app.route("/person/")
-def person():
-    return render_template("main.html", caption="Герои фильмов", link="Вернуться на главную страницу ")
+from forms import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
